34a2b2f688168d49114908a0f33907af14a5a714
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2022-10-06 17:15:47 +0900

    Initial commit (clean architecture 일부 적용)

---
+++ app/src/main/java/com/example/cryptocurrencyapp/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cryptocurrencyapp.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cryptocurrencyapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
